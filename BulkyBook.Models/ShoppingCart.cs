f4ad58df1a2951782473fd7c984d7404c06a173a
olasubomi <olasubomiodekunle@gmail.com>
2023-02-04 14:47:28 +0100

    ff

--- BulkyBook.Models/ShoppingCart.cs
+++ BulkyBook.Models/ShoppingCart.cs
@@ -1,6 +1,8 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace BulkyBook.Models;
 
