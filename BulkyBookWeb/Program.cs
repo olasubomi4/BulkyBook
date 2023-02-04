acf6baca4422cd35bdd104a1dd741bfd0495397a
Odekunle Olasubomi <40355148+olasubomi4@users.noreply.github.com>
2023-02-04 15:37:40 +0100

    Update Program.cs

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -43,7 +43,11 @@ builder.Services.AddSession(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
