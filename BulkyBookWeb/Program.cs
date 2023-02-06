2ba0326c9174a3e3bc272747b8af5c4ee7d20aa4
Odekunle Olasubomi <40355148+olasubomi4@users.noreply.github.com>
2023-02-06 01:52:07 +0100

    Update Program.cs

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -18,7 +18,6 @@ using BulkyBookWeb.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
